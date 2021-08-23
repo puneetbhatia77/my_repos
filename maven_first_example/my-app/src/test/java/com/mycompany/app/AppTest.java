47b3c6342ea8f0d107839d6e025abdf9317f8be5
AkashHadge <88318257+AkashHadge@users.noreply.github.com>
2021-08-23 15:37:32 +0530

    Update AppTest.java

--- maven_first_example/my-app/src/test/java/com/mycompany/app/AppTest.java
+++ maven_first_example/my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -3,7 +3,7 @@ package com.mycompany.app;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import junit.framework.akashTest;
+
 /**
  * Unit test for simple App.
  */
